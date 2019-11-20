ddefefc958ea01d0da100e08ef8e5177ca5ebbf1
Jo Jaramillo <jaramibr@oregonstate.edu>
2019-11-20 15:13:06 -0800

    Exercise 15 part 1
    Resume and data visualization

--- Jo Jaramillo Database/main.py
+++ Jo Jaramillo Database/main.py
@@ -5,25 +5,25 @@ import csv
 db = sqlite3.connect("zagster.sqlite3.db")
 with open('all.csv', 'r') as sample_csv:
  lines = csv.reader(sample_csv, delimiter=',')
- for row in lines:
- user_id = row[0]
- rental_id = row[1]
- start_lat = row[2]
- if start_lat == "":
- start_lat = "NULL"
- start_lon = row[3]
- if start_lon == "":
- start_lon = "NULL"
- end_lat = row[4]
- if end_lat == "":
- end_lat = "NULL"
- end_lon = row[5]
- if end_lon == "":
- end_lon = "NULL"
- start_time = row[6]
- end_time = row[7]
- membership_name = row[8]
- sql_insert = f"""
+    for row in lines:
+        user_id = row[0]
+        rental_id = row[1]
+        start_lat = row[2]
+        if start_lat == "":
+        start_lat = "NULL"
+        start_lon = row[3]
+        if start_lon == "":
+        start_lon = "NULL"
+        end_lat = row[4]
+        if end_lat == "":
+        end_lat = "NULL"
+        end_lon = row[5]
+        if end_lon == "":
+        end_lon = "NULL"
+        start_time = row[6]
+        end_time = row[7]
+        membership_name = row[8]
+    sql_insert = f"""
  INSERT INTO rides(user_id, rental_id, start_lat, start_lon,
  end_lat, end_lon, start_time, end_time,
  membership_name)
